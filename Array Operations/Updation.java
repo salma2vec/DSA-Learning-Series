98f45a205e1c7a9802cbd6aececa08077c1cf1fb
Salma <salmasaa02@gmail.com>
2021-04-03 23:40:56 +0530

    Update Updation.java

--- Array Operations/Updation.java
+++ Array Operations/Updation.java
@@ -1,5 +1,7 @@
-{
-  public static void main (String[]args) throws Exception
+import java.util.*;
+
+public class Main {
+  public static void main (String[] args) throws Exception
   {
     Scanner sc = new Scanner (System.in);
       System.out.println ("The number of elements in the array: ");
