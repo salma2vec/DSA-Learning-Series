c175e0e9e3cf1dd7443953424345e9852c8cf6df
Salma <salmasaa02@gmail.com>
2021-04-27 23:01:04 +0530

    Update Tree_Traversal.java

--- Binary Tree/Tree_Traversal.java
+++ Binary Tree/Tree_Traversal.java
@@ -93,3 +93,15 @@ public class Main {
         tree.printPostorder();
     }
 }
+
+
+/* Output:
+
+Preorder traversal of binary tree is
+1 2 4 5 3 
+Inorder traversal of binary tree is
+4 2 5 1 3 
+Postorder traversal of binary tree is
+4 5 2 3 1
+
+*/
