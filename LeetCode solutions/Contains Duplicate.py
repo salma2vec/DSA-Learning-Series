a83598d5577c3125d75e47334e7b20129fa7c4ff
Salma <salmasaa02@gmail.com>
2023-11-13 22:10:21 +0530

    Remove LeetCode solutions folder

--- LeetCode solutions/Contains Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        res = set()
-        for i in nums:
-            if i in res:
-                return True
-            res.add(i)
-        return False
